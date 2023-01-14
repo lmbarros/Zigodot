ef71de556e9663da1a06ae0184e50dd5c0c6a8ed
Leandro Motta Barros <lmb@stackedboxes.org>
2023-01-13 21:04:57 -0300

    We have a name -- use it!

--- src/main.zig
+++ src/main.zig
@@ -15,7 +15,7 @@ export fn the_test_init_function(p_interface: *const gd.GDNativeInterface, p_lib
     _ = p_library;
     _ = r_initialization;
 
-    std.debug.print("Hello from the extension!\n", .{});
+    std.debug.print("Hello from Zigodot!\n", .{});
     std.debug.print("Looks like we running Godot {}.{}.{}, AKA '{s}'.\n",
         .{ p_interface.version_major, p_interface.version_minor, p_interface.version_patch, p_interface.version_string });
 
